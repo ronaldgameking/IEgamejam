741c61151739941c39f10d98308d80ac611a3a31
Milan <milansietsma@gmail.com>
2021-02-02 15:47:23 +0100

    Adding my scene and made some UI things

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private int InfectionPoints = 15;
+
+    public Text InfectionPointsTxt; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateUI()
+    {
+        InfectionPointsTxt.text = InfectionPoints + "";
+    }
+}

714b77b59b7d989c50254f0ef17075649d6a9e8b
Milan <milansietsma@gmail.com>
2021-02-03 11:22:48 +0100

    Added the Syringe and cooldown to infect persons

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,23 +5,5 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public int InfectionPoints = 2;
 
-    public Text InfectionPointsTxt; 
-
-    void Start()
-    {
-        UpdateUI();
-    }
-
-    public void UpdateUI()
-    {
-        InfectionPointsTxt.text = InfectionPoints + "";
-    }
-
-    public void RemovePoints()
-    {
-        InfectionPoints--;
-        UpdateUI();
-    }
 }

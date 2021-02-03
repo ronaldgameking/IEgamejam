01cfe4cef69ae69e454703c7e1bdbd5bf36e8bd3
Milan <milansietsma@gmail.com>
2021-02-03 13:15:01 +0100

    Added Syringe

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -8,11 +8,22 @@ public class Clock : MonoBehaviour
     public float Speed = 4;
     public Transform handTransform;
 
+    public AudioManager Bell;
+
+    private void Start()
+    {
+        GameObject audio = GameObject.Find("GameManager");
+        Bell = audio.GetComponent<AudioManager>();
+    }
     void Update()
     {
         timer = (timer + Time.deltaTime * Speed) % 60;
 
         handTransform.rotation = Quaternion.Euler(0, 0, -timer * 6);
+        if (timer == 7.5f)
+        {
+            Bell.Play("Bell");
+        }
     }
 
 }

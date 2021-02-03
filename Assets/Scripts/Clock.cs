94555643368e387a17aa8c89d6d819f8936d364a
Milan <milansietsma@gmail.com>
2021-02-03 13:51:27 +0100

    Made the clock work. And added the sound bell

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -5,25 +5,59 @@ using UnityEngine;
 public class Clock : MonoBehaviour
 {
     public float timer;
+    public float quarterTimer = 0;
     public float Speed = 4;
     public Transform handTransform;
 
     public AudioManager Bell;
 
+    public ClockState currentclockstate;
+
     private void Start()
     {
+        currentclockstate = ClockState.Church;
         GameObject audio = GameObject.Find("GameManager");
         Bell = audio.GetComponent<AudioManager>();
     }
     void Update()
     {
         timer = (timer + Time.deltaTime * Speed) % 60;
+        quarterTimer += Time.deltaTime * Speed;
 
         handTransform.rotation = Quaternion.Euler(0, 0, -timer * 6);
-        if (timer == 7.5f)
+
+        if (quarterTimer >= 15)
         {
-            Bell.Play("Bell");
+            ChangeState();
         }
     }
 
+    public void PlayBell()
+    {
+        Bell.Play("Bell");
+    }
+
+    public void ChangeState()
+    {
+        if (timer >= 45)
+        {
+            currentclockstate = ClockState.Home;
+        }
+        else if (timer >= 30)
+        {
+            currentclockstate = ClockState.Fabric;
+        }
+        else if (timer >= 15)
+        {
+            currentclockstate = ClockState.Cafe;
+        }
+        else if (timer >= 0)
+        {
+            currentclockstate = ClockState.Church;
+        }
+        PlayBell();
+        quarterTimer -= 15;
+    }
+
 }
+public enum ClockState { Church, Cafe, Fabric, Home }

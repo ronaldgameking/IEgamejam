0e0025eca69328156805dd84a1b689560cbdc7ae
BakaRecca <eddie.norberg@gmail.com>
2021-02-04 09:25:52 +0100

    Fixed UI. (Please copy my scene and continue when you're ready.)

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -33,7 +33,7 @@ public class Character : MonoBehaviour
     {
         RestrictMovement(bounce);
         body.velocity = direction * moveSpeed;
-        spriteRenderer.sortingOrder = 5000 - Mathf.RoundToInt(transform.position.y);
+        spriteRenderer.sortingOrder = -Mathf.RoundToInt(transform.position.y);
     }
 
     private void RestrictMovement(int bounce)

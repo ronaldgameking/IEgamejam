460e6a9e493584dbd2dd68e597dcab2cb6061217
Milan <milansietsma@gmail.com>
2021-02-04 17:12:44 +0100

    CHANGES

--- Assets/Scripts/Human.cs
+++ Assets/Scripts/Human.cs
@@ -5,6 +5,7 @@ using Random = UnityEngine.Random;
 public class Human : Character
 {
     [SerializeField] private Zombie zombiePrefab;
+    private int Chance;
 
     protected override void Awake()
     {
@@ -20,6 +21,7 @@ public class Human : Character
     protected virtual void Update()
     {
         Move(1);
+        Chance = Random.Range(0, 3);
     }
 
     private void OnMouseDown()
@@ -30,7 +32,10 @@ public class Human : Character
 
     public void SetNewTarget()
     {
-        target = LevelManager.Instance.GetCurrentLocation();
+        if (Chance == 0)
+        {
+            target = LevelManager.Instance.GetCurrentLocation();
+        }
     }
 
     public void TurnToZombie()
@@ -39,20 +44,21 @@ public class Human : Character
         canMove = false;
         animator.SetTrigger("Turn");
     }
-    
+
     public void BecomeZombie()
     {
+        Debug.Log($"{name} Made a zombie");
         Character zombie = Instantiate(zombiePrefab, transform.position, Quaternion.identity).GetComponent<Character>();
         GameManager.Instance.RemoveCharacter(this, zombie);
         InfectionShot.Instance.Reset();
         Destroy(gameObject);
     }
-    
+
     protected override void OnTriggerEnter2D(Collider2D other)
     {
         if (isDead)
             return;
-        
+
         if (other.CompareTag("Zombie"))
             BecomeZombie();
     }
